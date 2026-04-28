7e6d604dcee8cb6d7e5d4504ead00e6e5b7dd158
Patone2B <antone.lomellini@hotmail.com>
2026-04-28 14:43:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+// MODE SOMBRE
 const btn = document.getElementById("themeToggle");
 
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    btn.textContent = "☀️ Mode clair";
-  } else {
-    btn.textContent = "🌙 Mode sombre";
-  }
+  btn.textContent = document.body.classList.contains("dark")
+    ? "☀️ Mode clair"
+    : "🌙 Mode sombre";
 });
+
+// FADE-IN SCROLL
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
